fddbdf9dc52aaf7a18c132f03a8be1c407cd9c27
rulo-dev <rulosan77@gmail.com>
2025-02-10 23:26:35 -0600

    creacion del proyecto con index y script, añadiendo API de geolocalizacion

---
+++ script.js
@@ -0,0 +1,8 @@
+const options = {
+  method: 'GET',
+  url: 'https://ip-geolocation-and-threat-detection.p.rapidapi.com/54.85.132.205',
+  headers: {
+    'X-RapidAPI-Key': '71e59e906dmshfdc9ed284df2469plb599cjsn0355f0f200d5',
+    'X-RapidAPI-Host': 'ip-geolocation-and-threat-detection.p.rapidapi.com',
+  },
+};
