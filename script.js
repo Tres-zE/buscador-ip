e2985138fb4d419d33469df3f1db734bc5f0222e
rulo-dev <rulosan77@gmail.com>
2025-02-11 23:26:07 -0600

    moficando index y script

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
-const options = {
+const OPTIONS = {
   method: 'GET',
-  url: 'https://ip-geolocation-and-threat-detection.p.rapidapi.com/54.85.132.205',
   headers: {
     'X-RapidAPI-Key': '71e59e906dmshfdc9ed284df2469plb599cjsn0355f0f200d5',
     'X-RapidAPI-Host': 'ip-geolocation-and-threat-detection.p.rapidapi.com',
   },
 };
+
+const fecthIpInfo = (ip) => {
+  fetch(
+    `https://ip-geolocation-and-threat-detection.p.rapidapi.com/${ip}`,
+    OPTIONS
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => console.error(error));
+};
+
+const $form = document.querySelector('#form');
+
+$form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const $input = document.querySelector('#ip');
+  fecthIpInfo($input.value);
+});
